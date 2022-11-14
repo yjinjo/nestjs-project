d67fb6050c681a56e7df2baf8800afd1d5ecc8a2
Youngjin Jo <yjinjo@berkeley.edu>
2022-11-14 11:53:56 +0900

    #2. Generics Recap

--- functions/functionTest.ts
+++ functions/functionTest.ts
@@ -1,10 +1,8 @@
-type SuperPrint = {
-  <TypePlaceholder>(arr: TypePlaceholder[]): TypePlaceholder
-}
+type SuperPrint = <T, V>(a: T[], b: V) => T
 
-const superPrint: SuperPrint = (arr) => arr[0]
+const superPrint: SuperPrint = (a) => a[0]
 
-const a = superPrint([1, 2, 3, 4])
-const b = superPrint([true, false, true, true])
-const c = superPrint(["a", "b", "c"])
-const d = superPrint([1, 2, true, false, "hello"])
+const a = superPrint([1, 2, 3, 4], "x")
+const b = superPrint([true, false, true, true], 3)
+const c = superPrint(["a", "b", "c"], false)
+const d = superPrint([1, 2, true, false, "hello"], [3, 2])
