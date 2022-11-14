ba53b85d9219791a46117f82224c6e79500dca67
Youngjin Jo <yjinjo@berkeley.edu>
2022-11-14 15:27:43 +0900

    #3. Polymorphism

--- OOP/classTest.ts
+++ OOP/classTest.ts
@@ -1,27 +1,27 @@
-interface User {
-  firstName: string
-  lastName: string
-
-  sayHi(name: string): string
-  fullName(): string
+interface SStorage<T> {
+  [key: string]: T
 }
 
-interface Human {
-  health: number
-}
-
-class Player implements User, Human {
-  constructor(
-    public firstName: string,
-    public lastName: string,
-    public health: number
-  ) { }
-
-  fullName() {
-    return `${this.firstName} ${this.lastName}`
+class LocalStorage<T> {
+  private storage: SStorage<T> = {}
+  set(key: string, value: T) {
+    this.storage[key] = value;
   }
-
-  sayHi(name: string) {
-    return `Hello ${name}. My name is ${this.fullName()}`
+  remove(key: string) {
+    delete this.storage[key]
+  }
+  get(key: string): T {
+    return this.storage[key]
+  }
+  clear() {
+    this.storage = {}
   }
 }
+
+const stringsStorage = new LocalStorage<string>()
+stringsStorage.get("ket")
+stringsStorage.set("hello", "how are you")
+
+const booleansStorage = new LocalStorage<boolean>()
+booleansStorage.get("xxx")
+booleansStorage.set("hello", true)
