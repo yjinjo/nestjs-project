b3e03a9ad5da6f2947027c2b50cbb7896e2529bd
Youngjin Jo <yjinjo@berkeley.edu>
2022-11-14 17:48:38 +0900

    #4. Blocks

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,27 @@
 // index.ts
 
-import { init, exit } from "./myPackage";
+import crypto from "crypto";
+
+interface BlockShape {
+  hash: string;
+  prevHash: string;
+  height: number;  // 블록의 위치를 표시해줌 
+  data: string;    // 보호하길 원하는 데이터 
+}
+
+class Block implements BlockShape {
+  public hash: string;
+
+  constructor(
+    public prevHash: string,
+    public height: number,
+    public data: string
+  ) {
+    this.hash = Block.calculateHash(prevHash, height, data);
+  }
+
+  static calculateHash(prevHash: string, height: number, data: string) {
+    const toHash = `${prevHash}${height}${data}`
+
+  }
+}
